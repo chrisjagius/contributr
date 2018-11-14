e43b46ac5939b0cd4fe9ded7f0d0c199d797a34f
Chris Agius <cagius@2u.com>
2018-11-14 13:35:43 -0500

    unstashed changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Page from "./components/Page";
 // import Search from "./components/Search";
 import ResultList from "./components/ResultList";
 
+import * as repoJson from "./tmp/sampleData.json";
+
 class App extends Component {
   state = {
-    results: ["foo", "bar"]
+    results: repoJson["data"]["repositoryOwner"]["repositories"]
   };
 
   render() {
+    console.log(this.state.results);
+
     return (
       <Page>
-        {/* <Search /> */}
-        <ResultList results={this.state.results} />
+        <Search />
+        {/* <ResultList results={this.state.results} /> */}
       </Page>
     );
   }
---
+++ src/adapters/GithubAdapter.js
@@ -0,0 +1,101 @@
+const fetcher = require("isomorphic-fetch");
+const CONFIG = require("../../config.json");
+
+let cursor;
+
+const queryFragment = `
+pageInfo {
+  hasNextPage
+  hasPreviousPage
+  endCursor
+  startCursor
+}
+edges {
+  cursor
+  node {
+    name
+    languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+    issues(first: 100, labels: $labels) {
+      edges {
+        node {
+          title
+          number
+          url
+          closed
+        }
+      }
+    }
+  }
+}
+}
+}
+}
+`;
+
+const initialQuery = `
+query ($username: String!, $labels: [String!]) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC) {
+`;
+
+const nextQuery = `
+query ($username: String!, $labels: [String!]) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC, after: ${cursor}) {
+`;
+
+const previousQuery = `
+query ($username: String!, $labels: [String!]) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC, before: ${cursor}) {
+`;
+
+const variables = {
+  username: "SaigeXSaige",
+  labels: [
+    "first timers welcome",
+    "first-timers-only",
+    "Level:Starter",
+    "good first issue",
+    "beginner",
+    "good for beginner",
+    "starter bug",
+    "Good for New Contributors",
+    "good-first-contribution"
+  ]
+};
+
+class GithubAdapter {
+  static runQuery(
+    query,
+    { headers = {}, variables = {}, fetcher = fetch } = {}
+  ) {
+    let body = { query };
+
+    if (Object.keys(variables).length) {
+      body.variables = variables;
+    }
+
+    return fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: Object.assign(
+        {
+          Authorization: "token " + CONFIG.apiKey
+        },
+        headers
+      ),
+      body: JSON.stringify(body)
+    }).then(res => res.json());
+  }
+}
+
+GithubAdapter.runQuery(initialQuery + queryFragment, {
+  variables,
+  fetcher
+}).then(console.log);
