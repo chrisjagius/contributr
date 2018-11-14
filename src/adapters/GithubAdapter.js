447db50032cdde6826b91b54bdf88cc4e02cf328
Saige Leslie <saigeleslie@gmail.com>
2018-11-14 14:41:52 -0500

    Set cursor variable on every GithubAdapter method

--- src/adapters/GithubAdapter.js
+++ src/adapters/GithubAdapter.js
@@ -90,9 +90,52 @@ function runQuery(
     body: JSON.stringify(body),
   }).then(res => res.json())
 }
+
+function setCursor(data) {
+  const { pageInfo } = data
+  if (pageInfo.hasNextPage) {
+    cursor = pageInfo.endCursor
+  }
+  if (pageInfo.hasPreviousPage) {
+    cursor = pageInfo.startCursor
+  }
+
+  return data
 }
 
-GithubAdapter.runQuery(initialQuery + queryFragment, {
-  variables,
-  fetcher,
-}).then(console.log)
+export default class GithubAdapter {
+  static getRepos() {
+    const variables = Object.assign({}, queryVariables)
+    return runQuery(initialQuery + queryFragment, {
+      variables,
+      fetcher,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+
+  static getNextRepos() {
+    const variables = Object.assign({ cursor }, queryVariables)
+    return runQuery(nextQuery + queryFragment, {
+      variables,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+
+  static getPrevRepos() {
+    const variables = Object.assign({ cursor }, queryVariables)
+    return runQuery(previousQuery + queryFragment, {
+      variables,
+      fetcher,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+}
+
+// console.log({ cursor })
+// GithubAdapter.getRepos()
+//   .then(() => console.log({ cursor }))
+//   .then(GithubAdapter.getNextRepos)
+//   .then(console.log)
