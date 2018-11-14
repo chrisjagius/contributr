4873adc2bab0a4d42e684391aa96c1625c3671d5
adiallo <adiallo@2u.com>
2018-11-14 12:53:02 -0500

    Add styling to page and search components

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  grid-template-columns: auto 1fr;
+  justify-content: space-between;
+  align-items: stretch;
+  font-size: 6rem;
+  color: #0078D7;
+`;
 
 const Page = props => {
   return (
-    <div>
-      <h1>Contributr</h1>
+    <Center>
+      <StyledHeader>Contributr</StyledHeader>
       {props.children}
-    </div>
+    </Center>
   );
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,27 @@
 import React from "react";
+import styled from 'styled-components'
 
+const StyledInput = styled.input`
+  justify-content: center;
+  height: 50px;
+  width: 500px;
+  padding-left: 20px;
+  font-size: 0.75rem;
+`
+
+const StyledButton = styled.button`
+  justify-content: center;
+  height: 3.5rem;
+  width: 4rem;
+  color: white;
+  background-color: #0078D7;
+  font-size: 1rem;
+`
 const Search = () => {
   return (
     <div>
-      <select>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
-      </select>
-
-      {/* <input type="text" placeholder="Search.." />
-      <button type="submit">Search</button> */}
+      <StyledInput type="text" placeholder="Enter Project name..." />
+      <StyledButton type="submit">Search</StyledButton>
     </div>
   );
 };
