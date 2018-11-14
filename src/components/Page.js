aae8a21f7b627a46363ca31559e529006e364558
adiallo <adiallo@2u.com>
2018-11-14 14:54:02 -0500

    Change front-size to 5rem

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 
-const Center = styled.div`
-  text-align: center;
-`;
+const Center = styled.div`text-align: center;`;
 
 const StyledHeader = styled.header`
-  grid-template-columns: auto 1fr;
-  justify-content: space-between;
-  align-items: stretch;
-  font-size: 6rem;
-  color: #0078D7;
+	margin: 2rem;
+	grid-template-columns: auto 1fr;
+	justify-content: space-between;
+	align-items: stretch;
+	font-size: 5rem;
+	color: #0078d7;
 `;
 
-const Page = props => {
-  return (
-    <Center>
-      <StyledHeader>Contributr</StyledHeader>
-      {props.children}
-    </Center>
-  );
+const Page = (props) => {
+	return (
+		<Center>
+			<StyledHeader>Contributr</StyledHeader>
+			{props.children}
+		</Center>
+	);
 };
 
 export default Page;
