19d9fade5568f065f1a7db4c3a46b78efe0383ae
Abdoul Diallo <diallo.abdoulrahime@gmail.com>
2018-11-14 14:49:27 -0500

    Adapter (#1)
    * Add config to hold Github API key
    
    * Ignore config.json file
    
    * Add isomorphic-fetch and es6-promise for development
    
    * Set up an adapter file with a function to fetch from Github's graphql API
    
    * Add endCursor and startCursor to the queryFragment
    
    * Use the GithubAdapter class
    
    * Use cursor variable instead of string interpolation
    
    * (m) Update default variables
    
    * Make runQuery a pure fn
    
    * Set cursor variable on every GithubAdapter method
    
    * Pass username dynamically to variables

---
+++ src/adapters/GithubAdapter.js
@@ -0,0 +1,140 @@
+const fetcher = require('isomorphic-fetch')
+const CONFIG = require('../../config.json')
+
+let cursor
+
+const queryFragment = `
+pageInfo {
+  hasNextPage
+  hasPreviousPage
+  endCursor
+  startCursor
+}
+nodes {
+    name
+    languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+    issues(first: 100, labels: $labels) {
+      edges {
+        node {
+          title
+          number
+          url
+          closed
+        }
+      }
+    }
+  }
+}
+}
+}
+`
+
+const initialQuery = `
+query ($username: String!, $labels: [String!]) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC) {
+`
+
+const nextQuery = `
+query ($username: String!, $labels: [String!], $cursor: String!) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC, after: $cursor) {
+`
+
+const previousQuery = `
+query ($username: String!, $labels: [String!], $cursor: String!) {
+  repositoryOwner(login: $username) {
+    repositories(first: 100, privacy: PUBLIC, before: $cursor) {
+`
+
+const queryVariables = {
+  labels: [
+    'first timers welcome',
+    'first-timers-only',
+    'Level:Starter',
+    'good first issue',
+    'beginner',
+    'good for beginner',
+    'starter bug',
+    'Good for New Contributors',
+    'good-first-contribution',
+    'help wanted',
+  ],
+}
+
+function runQuery(
+  query,
+  { headers = {}, variables = {}, fetcher = fetch } = {}
+) {
+  let body = { query }
+
+  if (Object.keys(variables).length) {
+    body.variables = variables
+  }
+
+  return fetcher('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: Object.assign(
+      {
+        Authorization: 'token ' + CONFIG.apiKey,
+      },
+      headers
+    ),
+    body: JSON.stringify(body),
+  }).then(res => res.json())
+}
+
+function setCursor(data) {
+  const { pageInfo } = data
+  if (pageInfo.hasNextPage) {
+    cursor = pageInfo.endCursor
+  }
+  if (pageInfo.hasPreviousPage) {
+    cursor = pageInfo.startCursor
+  }
+
+  return data
+}
+
+export default class GithubAdapter {
+  static getRepos(username) {
+    const variables = Object.assign({ username }, queryVariables)
+    return runQuery(initialQuery + queryFragment, {
+      variables,
+      fetcher,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+
+  static getNextRepos() {
+    const variables = Object.assign({ cursor }, queryVariables)
+    return runQuery(nextQuery + queryFragment, {
+      variables,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+
+  static getPrevRepos() {
+    const variables = Object.assign({ cursor }, queryVariables)
+    return runQuery(previousQuery + queryFragment, {
+      variables,
+      fetcher,
+    })
+      .then(resp => resp.data.repositoryOwner.repositories)
+      .then(setCursor)
+  }
+}
+
+// console.log({ cursor })
+// GithubAdapter.getRepos('FreeCodeCamp')
+//   .then(() => console.log({ cursor }))
+//   .then(GithubAdapter.getNextRepos)
+//   .then(console.log)
